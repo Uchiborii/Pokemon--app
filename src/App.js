bd69999b0ba4528a8b90cd839b197e99d3474d5c
uchiborieri <zuumo219@gmail.com>
2024-06-01 10:19:11 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { getAllPokemon } from "./utils/pokemon.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      try {
+        // 全てのポケモンデータを取得
+        const res = await getAllPokemon(initialURL);
+        console.log(res);
+      } catch (error) {
+        console.error("Error fetching Pokemon data:", error);
+      }
+    };
+    fetchPokemonData();
+  }, []);
+
+  return <div className="App">Pokemon App</div>;
 }
 
 export default App;
